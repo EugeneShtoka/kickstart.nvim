0b06e13567be42436d353cab52f6e880b09bfc8d
Eugene Shtoka <eshtoka@gmail.com>
2026-02-13 15:07:40 +0200

    update

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,10 +11,24 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
+  init = function()
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        if vim.fn.argc() == 0 then
+          vim.cmd 'Neotree show'
+        end
+      end,
+    })
+  end,
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_gitignored = true,
+        hide_dotfiles = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
